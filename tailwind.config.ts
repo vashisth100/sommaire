9ac366239732baa0e0c3e21933b02bf9af3bd5fa
vashisth100 <priyanshuvashisth102@gmail.com>
2025-04-04 23:03:23 +0530

    project init

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+        colors: {
+            background: "var(--background)",
+            foreground: "var(--foreground)",
+        },
+    },
+  },
+  plugins: [],
+} satisfies Config;
